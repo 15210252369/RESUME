09caa2efea68e7738200536c92ab51392bc93d00
杨国玉 <516433517@qq.com>
2019-06-23 15:12:11 +0800

    myresume

--- js/index.js
+++ js/index.js
@@ -350,9 +350,23 @@ let detailRender = (function () {
     }
 })()
 
-$('body').on('touchstart touchmove touchend', (ev) => {
-    ev.preventDefault();
+var startY, endY;
+//记录手指触摸的起点坐标
+$('body').on('touchstart', function (e) {
+    startY = e.touches[0].pageY;
 });
+$('body').on('touchmove', function (e) {
+    endY = e.touches[0].pageY;  //记录手指触摸的移动中的坐标
+    //手指下滑，页面到达顶端不能继续下滑
+    if (endY > startY && $(window).scrollTop() <= 0) {
+        e.preventDefault();
+    }
+    //手指上滑，页面到达底部能继续上滑
+    if (endY < startY && $(window).scrollTop() +
+        $(window).height() >= $('body')[0].scrollHeight) {
+        e.preventDefault();
+    }
+})
 
 let url = window.location.href;
 let well = url.indexOf('#')
